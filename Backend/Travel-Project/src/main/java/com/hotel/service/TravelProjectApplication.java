2384be7fcea7a888d176ae95fbf4cbc9914c259e
sanyam <sanyamnarang40@gmail.com>
2023-08-24 10:21:41 +0530

    backend added

---
+++ Backend/Travel-Project/src/main/java/com/hotel/service/TravelProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelProjectApplication.class, args);
+	}
+
+}
